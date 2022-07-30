68b3423e74bf54699285a378218a7d843778fd39
Jeineffer Kauany Ribeiro <jeineffer_@hotmail.com>
2022-07-30 17:20:58 -0300

    update uptade

--- apex/aula006/arquivos.py
+++ apex/aula006/arquivos.py
@@ -5,3 +5,17 @@
 arquivo = open('nomes.txt', 'w')
 arquivo.write('Maycon')
 arquivo.close()
+
+# x = abre em modo de escrita,
+# se o arquivo não existir cria, se existir dá erro
+
+# arquivo = open('numeros.txt', 'x')
+# arquivo.write('4')
+# arquivo.close()
+
+# a = abre em modo de escrita,
+# se o arquivo não existir cria, se existir adiciona dados ao final
+
+arquivo = open('bandas.txt', 'a')
+arquivo.write('PinkFloyd')
+arquivo.close()
