d489711cbcc5dc82b191f983c175c337744cd008
Jeineffer Kauany Ribeiro <jeineffer_@hotmail.com>
2022-09-15 22:21:59 -0300

    Iist-Comprehension

---
+++ python_programming/logica/listComprehension.py
@@ -0,0 +1,7 @@
+lista_produtos = ['celular', 'tablet', 'fone de ouvido', 'camera','microfone']
+lista_precos = [1000, 1200, 300, 500, 600]
+
+lista_precos_reajuste = [preco * 1.1 for preco in lista_precos]
+
+print(lista_precos_reajuste)
+
