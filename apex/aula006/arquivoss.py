ddee3aca6e404b4d685d2759311e014c02f8c49a
Jeineffer Kauany Ribeiro <jeineffer_@hotmail.com>
2022-09-17 15:02:50 -0300

    update
    update

--- apex/aula006/arquivoss.py
+++
@@ -1,8 +0,0 @@
-arquivoss = open ('Aula006/pessoas.txt', 'a') #abre o arquivo no formato de escrita colocando o conteúdo no final do arquivo
-arquivoss.write('Davi\n')# insere um texto com quebra de linha 
-arquivoss.close()#fecha o arquivo 
-
-arquivi = open (Aula006/'pessoa.txt' , 'r') #abre o formato de leitura 
-for linha in arquivoss:
-    linha = Linha.strip()
-    dados = Linha.split(';')
